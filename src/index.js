701dfca3808683a99c18b7f69197069894b032bb
Luke Jackson <lukejacksonn@gmail.com>
2018-01-05 17:03:54 +0000

    Bumps hyperapp version

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 import { app } from 'hyperapp'
 import { main, h1, div, button } from '@hyperapp/html'
 
-app({
-  state: {
-    count: 0,
-  },
-  actions: {
-    reset: () => ({ count: 0 }),
-    add: state => ({ count: state.count + 1 }),
-    sub: state => ({ count: state.count - 1 }),
-  },
-  view: (state, actions) =>
-    main([
-      h1(state.count),
-      div([
-        button({ onclick: actions.sub }, 'Sub'),
-        button({ onclick: actions.reset }, 'Reset'),
-        button({ onclick: actions.add }, 'Add'),
-      ]),
+const state = {
+  count: 0,
+}
+
+const actions = {
+  reset: () => ({ count: 0 }),
+  sum: data => ({ count }) => ({ count: count + data }),
+}
+
+const view = (state, actions) =>
+  main([
+    h1(state.count),
+    div([
+      button({ onclick: e => actions.sum(-1) }, 'Sub'),
+      button({ onclick: e => actions.reset() }, 'Reset'),
+      button({ onclick: e => actions.sum(1) }, 'Add'),
     ]),
-})
+  ])
+
+app(state, actions, view, document.body)
